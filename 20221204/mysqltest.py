5dcdc4bdb25a8c6c774270c4d3a34e5045ab8e26
chenhui0923 <251387518@qq.com>
2023-07-18 16:47:40 +0800

    1

---
+++ 20221204/mysqltest.py
@@ -0,0 +1,19 @@
+
+import pymysql;
+db = pymysql.connect(host='192.168.8.249', user='pingtt', port=3306, password='qwer1234~!')
+
+cursor = db.cursor()
+sql = "SELECT * FROM `exhibition`.`proofing_notice`"
+
+try:
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for row in results:
+        id =row[0]
+        color_id = row[2]
+        print(id,color_id)
+except:
+    db.rollback()
+
+# 关闭数据库连接
+db.close();
