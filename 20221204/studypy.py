f40f69de3ed2575461ecf891568349afec17104d
chenhui0923 <251387518@qq.com>
2023-07-27 17:14:51 +0800

    1

---
+++ 20221204/studypy.py
@@ -0,0 +1,10 @@
+list1 = ['sty', 'school']
+numbers1 = len(list1)  # 计算列表元素个数
+numbers2 = list1[0]  # 返回第0个元素
+list1.append('red')  # 在尾部添加一个元素
+list1.extend(['blue', 'yellow'])  # 在尾部添加多个元素
+list1.insert(1, 'blue')  # 在1的位置插入元素 blue
+list1.remove('blue') #删除列表中的第一个为blue的元素
+list1.pop(0) #删除
+print(numbers1, numbers2)
+print(list1)
