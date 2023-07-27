6128a594b3966032f5ce9f02d2ef23cfbaaaed5a
chenhui0923 <251387518@qq.com>
2023-07-27 17:54:00 +0800

    1

--- 20221204/studypy.py
+++ 20221204/studypy.py
@@ -5,6 +5,9 @@ list1.append('red')  # 在尾部添加一个元素
 list1.extend(['blue', 'yellow'])  # 在尾部添加多个元素
 list1.insert(1, 'blue')  # 在1的位置插入元素 blue
 list1.remove('blue') #删除列表中的第一个为blue的元素
-list1.pop(0) #删除
+list1.pop(0) #删除第0个元素
+
+list1.clear() #清空列表
 print(numbers1, numbers2)
 print(list1)
+
