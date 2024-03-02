0740285193cd6c6c4c7b17f9956f94b32957b60c
chenhui0923 <251387518@qq.com>
2024-03-02 18:38:23 +0800

    学习

--- 20240218/excel.py
+++ 20240218/excel.py
@@ -4,14 +4,14 @@ import pandas as pd
 # pip install Pyarrow --target=D:\pythonProject\venv\Lib\site-packages
 # pip install openpyxl --target=D:\pythonProject\venv\Lib\site-packages
 
-
+# 学习https://c.biancheng.net/pandas/excel.html
 
 def writeExcel():
 #创建DataFrame数据
-     info_website = pd.DataFrame({'name': ['编程帮', 'c语言中文网', '微学苑', '92python'],
-          'rank': [1, 2, 3, 4],
-          'language': ['PHP', 'C', 'PHP','Python' ],
-          '': ['www.bianchneg.com', 'c.bianchneg.net', 'www.weixueyuan.com','www.92python.com' ]})
+     info_website = pd.DataFrame({'名称': ['编程帮', 'c语言中文网', '微学苑', '92python'],
+          '等级': [1, 2, 3, 4],
+          '语言': ['PHP', 'C', 'PHP','Python' ],
+          '网址': ['www.bianchneg.com', 'c.bianchneg.net', 'www.weixueyuan.com','www.92python.com' ]})
      #创建ExcelWrite对象
      writer = pd.ExcelWriter('website.xlsx')
      info_website.to_excel(writer)
@@ -19,9 +19,11 @@ def writeExcel():
      print('输出成功')
 
 def readExcel():
-     df = pd.read_excel('website.xlsx',index_col='name')
-     df.columns = df.columns.str.replace('Unnamed: 0', 'col_label')
+     df = pd.read_excel('website.xlsx',index_col='名称')
+     df.columns = df.columns.str.replace('Unnamed: 0', '序号')
      print(df)
 
-# writeExcel()
-readExcel()
+
+
+writeExcel()
+# readExcel()
