411058533e09f879b30671adeb3ad211aa1d8e72
chenhui0923 <251387518@qq.com>
2024-04-16 17:39:26 +0800

    学习

---
+++ 20240416/wenjian.py
@@ -0,0 +1,51 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+
+
+
+def main(file_path):
+    if not os.path.isdir(file_path):
+        raise ValueError(f"{file_path} is not a directory.") #判断是文件夹
+
+    pdf_files = glob.glob(os.path.join(file_path, "*.pdf"))
+    filenames = [os.path.basename(pdf) for pdf in pdf_files]
+    for files in filenames:
+        # print(files)
+        sptext = files.split("_")[1]
+        # print(sptext,files)
+        text = (sptext,files)
+        file1.append(text)
+
+
+def excel(excel_path):
+    df = pd.read_excel(excel_path)
+    column_data = df['发票号'].values.tolist()
+    # print(column_data)
+    file2.append(column_data)
+
+
+def arry():
+    first = []
+    for sub in file1:
+        first.append(sub[0])
+    inters = np.intersect1d(first,file2)
+    # print(inters)
+    for arr in file1:
+        if inters in arr:
+            print(arr[1])
+
+if __name__ == '__main__':
+    file1 = []  #用来看所提取的文件名
+    file2 = []  #用于存储excel 文件中的发票号
+    # file3 = []  #上面两个数据的交集
+    file_path = r"C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\测试数据\202403"
+    excel_path = r"C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\测试数据\票号.xlsx"
+    # file_path = input("输入所需提取的文件夹路径：")
+    # excel_path = input("输入所需提取的excel文件")
+    main(file_path)
+    excel(excel_path)
+    arry()
+    # print(file1)
+
--- sqlserver/diaogua.py
+++ sqlserver/diaogua.py
@@ -1,11 +1,76 @@
-import pyodbc
-# py链接sqlserver服务器
-conn = pyodbc.connect('DRIVER={SQL Server};SERVER=192.168.100.189,1433;DATABASE=jack;UID=sa;PWD=jack123')
-print("数据库连接:", conn)
+import pyodbc  # py链接sqlserver服务器
+import pymysql;  # py链接mysql服务器
+from datetime import  datetime
+
+
+
+
+
+conn = pyodbc.connect('DRIVER={SQL Server};SERVER=192.168.100.190,1433;DATABASE=LDEXCH;UID=sa;PWD=jack123')
+db = pymysql.connect(host='192.168.9.220', user='middle', port=3306, password='middle')
+
+
 cur = conn.cursor()
+cursor = db.cursor()
+
+
 if not cur:
     raise (NameError, "数据库连接失败")
-cur.execute("SELECT  * from tColor")
-resList = cur.fetchall()
-conn.close()
-print(resList)
+
+
+def diaogua_190():
+    sql = "SELECT  * from tOWorkSum where WorkLine=103"
+
+    try:
+        cur.execute(sql)
+        resList = cur.fetchall()
+        for row in resList:
+            ShtDate = row[1]    # 生产日期
+            IsOT    = row[2]    # 加班否
+            MONo    = row[3]    # 款号
+            ColorNo = row[7]    #色号
+            SeqNo   = row[12]   #工序序号
+            SeqName = row[14]  #工序名称
+            Qty     = row[24]   #数量
+            WorkLine =  row[19] #哪个工作站
+            text1= (ShtDate,IsOT,MONo,ColorNo,SeqNo,SeqName,Qty,WorkLine)
+            print(text1)
+        conn.commit()
+    except:
+        conn.rollback()
+
+
+def jishudiaogua_220():
+    sql = "SELECT process_date,style_no,color,size,process_name,staff_name,quantity FROM middle.dg_daily_record a where a.process_last=1 and a.process_date='2024-04-10'"
+    shuzu=[]
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+
+        for row in results:
+            process_date = row[0]   #日期
+            style_no = row[1]       #款号
+            color = row[2]          #色号
+            size = row[3]           #尺码
+            process_name = row[4]   #工序名
+            staff_name = row[5]     #操作员
+            quantity = row[6]       #数量
+            day = process_date.strftime('%Y%m%d')
+            test1 = (day,style_no,color,size,process_name,staff_name,quantity)
+            # shuzu.append(test1)
+            print(test1)
+        db.commit()
+    except:
+        db.rollback()
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # diaogua_190();
+    jishudiaogua_220();
+
+    conn.close()
