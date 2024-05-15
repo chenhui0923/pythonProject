d09f2783539547963c2aa606e25219f831ff665e
chenhui0923 <251387518@qq.com>
2024-05-15 23:02:12 +0800

    写完了

--- 20240416/wenjian.py
+++ 20240416/wenjian.py
@@ -5,27 +5,6 @@ import shutil
 
 
 
-def main(path):
-    files = os.listdir(path)
-    for file in files:
-        if os.path.isdir(path + "\\" + file):
-            search_dir(path + "\\" + file)
-        else:
-            if file.endswith('.pdf'):
-                file_path1.append(file)
-
-    print(file_path1)
-    filenames = [os.path.basename(pdf) for pdf in file_path1]
-    for files in filenames:
-        sptext = files.split("_")[1]
-        # print(sptext,file2)
-        if sptext in file2[0]:
-            old_file = file_path + "/" + files
-            dst_file = os.path.join(qianyi_path,os.path.basename(files))
-            print(old_file,dst_file)
-            # shutil.copyfile(old_file,dst_file)
-
-
 def excel(excel_path):
     df = pd.read_excel(excel_path)
     column_data = df['发票号'].values.tolist()
@@ -33,34 +12,57 @@ def excel(excel_path):
     file2.append(column_data)
 
 def search_dir(path):
-    files = os.listdir(path)
-    for file in files:
-        if os.path.isdir(path+"\\"+file):
-            search_dir(path + "\\" + file)
+    for entry in os.listdir(path):
+        entry_path = os.path.join(path, entry)
+        if os.path.isdir(entry_path):
+            search_dir(entry_path)  # 递归搜索子目录
         else:
-            if file.endswith('.pdf'):
-                file_path1.append(path + "\\" + file)
+            if entry_path.endswith('.pdf'):
+                file_path1.append(entry_path);
+    for pdf in file_path1:
+        filenames = os.path.basename(pdf)
+        # print(filenames,pdf)
+        sptext = filenames.split("_")[1]
+        if sptext in file2[0]:
+            old_file = pdf
+            dst_file = os.path.join(qianyi_path,os.path.basename(filenames))
+            shutil.copyfile(old_file, dst_file)
 
-    print(file_path1)
+def EXCELPATH():
+    testqw=[]
+    nerong = []
+    for pdf in file_path1:
+        filenames = os.path.basename(pdf)
+        sptext = filenames.split("_")[1]
+        testqw.append(sptext)
 
-
+    for sptext1 in file2[0]:
+        if sptext1 not in testqw:
+            nerong.append(sptext1)
+    print(nerong)
+    info_website = pd.DataFrame({'缺失发票数据': nerong})
+    writer = pd.ExcelWriter('缺失发票数据.xlsx')
+    info_website.to_excel(writer)
+    writer.close()
+    print('输出成功')
 if __name__ == '__main__':
     file_path1 = []  #用来看所提取的文件名
     file2 = []  #用于存储excel 文件中的发票号
-    # file3 = []  #上面两个数据的交集
+    file3 = []  #处理不存在的发票号
 
-    file_path = r"C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\测试数据"
-    # file_path = r"C:/Users/Administrator/Desktop/测试数据/202403"
+    # file_path = r"C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\测试数据"
+    # file_path = r"C:\Users\Administrator\Desktop\测试数据"
 
-    excel_path = r"C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\测试数据\票号.xlsx"
-    # excel_path = r"C:/Users/Administrator/Desktop/测试数据/票号.xlsx"
-    qianyi_path = r'C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\迁移文件夹'
+    # excel_path = r"C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\测试数据\票号.xlsx"
+    # excel_path = r"C:\Users\Administrator\Desktop\测试数据\票号.xlsx"
+    # qianyi_path = r'C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\迁移文件夹'
+    # qianyi_path = r'C:\Users\Administrator\Desktop\迁移'
 
     #
-    # file_path = input("输入所需提取的文件夹路径：")
-    # excel_path = input("输入所需提取的excel文件：")
-    # qianyi_path = input("输入迁移的目标文件夹：")
+    file_path = input("输入所需提取的文件夹路径：")
+    excel_path = input("输入所需提取的excel文件：")
+    qianyi_path = input("输入迁移的目标文件夹：")
 
     excel(excel_path)
     search_dir(file_path);
-    # main(file_path)
+    EXCELPATH();
