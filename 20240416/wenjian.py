6430fdfeaa35d91b34035989c7d3f39f352946a6
chenhui0923 <251387518@qq.com>
2024-05-15 14:22:29 +0800

    学习

--- 20240416/wenjian.py
+++ 20240416/wenjian.py
@@ -5,20 +5,25 @@ import shutil
 
 
 
-def main(file_path,file2):
-    if not os.path.isdir(file_path):
-        raise ValueError(f"{file_path} is not a directory.") #判断是文件夹
+def main(path):
+    files = os.listdir(path)
+    for file in files:
+        if os.path.isdir(path + "\\" + file):
+            search_dir(path + "\\" + file)
+        else:
+            if file.endswith('.pdf'):
+                file_path1.append(file)
 
-    pdf_files = glob.glob(os.path.join(file_path, "*.pdf"))
-    filenames = [os.path.basename(pdf) for pdf in pdf_files]
+    print(file_path1)
+    filenames = [os.path.basename(pdf) for pdf in file_path1]
     for files in filenames:
         sptext = files.split("_")[1]
         # print(sptext,file2)
         if sptext in file2[0]:
             old_file = file_path + "/" + files
             dst_file = os.path.join(qianyi_path,os.path.basename(files))
-            # print(old_file,dst_file)
-            shutil.copyfile(old_file,dst_file)
+            print(old_file,dst_file)
+            # shutil.copyfile(old_file,dst_file)
 
 
 def excel(excel_path):
@@ -27,33 +32,35 @@ def excel(excel_path):
     # print(column_data)
     file2.append(column_data)
 
+def search_dir(path):
+    files = os.listdir(path)
+    for file in files:
+        if os.path.isdir(path+"\\"+file):
+            search_dir(path + "\\" + file)
+        else:
+            if file.endswith('.pdf'):
+                file_path1.append(path + "\\" + file)
 
-# def arry():
-#     first = []
-#     for sub in file1:
-#         first.append(sub[0])
-#     inters = np.intersect1d(first,file2)
-#     print(inters,file1)
-#     matchs = find
+    print(file_path1)
+
 
 if __name__ == '__main__':
-    file1 = []  #用来看所提取的文件名
+    file_path1 = []  #用来看所提取的文件名
     file2 = []  #用于存储excel 文件中的发票号
     # file3 = []  #上面两个数据的交集
 
-    # file_path = r"C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\测试数据\202403"
+    file_path = r"C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\测试数据"
     # file_path = r"C:/Users/Administrator/Desktop/测试数据/202403"
 
-    # excel_path = r"C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\测试数据\票号.xlsx"
+    excel_path = r"C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\测试数据\票号.xlsx"
     # excel_path = r"C:/Users/Administrator/Desktop/测试数据/票号.xlsx"
-    # qianyi_path = r'C:/Users/Administrator/Desktop/迁移文件夹'
-    file_path = input("输入所需提取的文件夹路径：")
-    excel_path = input("输入所需提取的excel文件")
-    qianyi_path = input("输入迁移的目标文件夹")
+    qianyi_path = r'C:\Users\Administrator.DESKTOP-JLQ33MI\Desktop\迁移文件夹'
+
+    #
+    # file_path = input("输入所需提取的文件夹路径：")
+    # excel_path = input("输入所需提取的excel文件：")
+    # qianyi_path = input("输入迁移的目标文件夹：")
+
     excel(excel_path)
-
-    main(file_path,file2)
-
-    # arry()
-    print(file1)
-
+    search_dir(file_path);
+    # main(file_path)
